4a67a850f1333fdc19eadc0d5c58674ff6b0cc8a
Aparna Halder <aparna.halder82@gmail.com>
2022-08-10 10:12:41 +0000

    changes made from theia lab environment

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
-#from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
+#from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
-#class QuestionInline(admin.StackedInline):
-#    model = Question
+class QuestionInline(admin.StackedInline):
+    model = Question
 
-#class ChoiceInline(admin.StackedInline):
-#    model = Choice
-#    extra = 2
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
 
 
 class LessonInline(admin.StackedInline):
@@ -32,9 +32,9 @@ class LessonAdmin(admin.ModelAdmin):
 #class ChoiceAdmin(admin.ModelAdmin):
     #list_display = ('question', 'choice_text','is_correct')
 
-#class QuestionAdmin(admin.ModelAdmin):
- #   inlines = [ChoiceInLine]
- #   list_display = ('course', 'lesson', 'question_text', 'grade')
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInLine]
+    list_display = ('course', 'lesson', 'question_text', 'grade')
 
 
 
@@ -42,8 +42,9 @@ class LessonAdmin(admin.ModelAdmin):
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
-#admin.site.register(Question, QuestionAdmin)
+admin.site.register(Question, QuestionAdmin)
 #admin.site.register(Choice, ChoiceAdmin)
-#admin.site.register(Submission)
+admin.site.register(Choice)
+admin.site.register(Submission)
 admin.site.register(Instructor)
 admin.site.register(Learner)
